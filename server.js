b0a5ba85a42881fb3b70a814dd774913df25cf61
AnLangThang81 <nguyenhoaian2821@gmail.com>
2026-01-15 19:09:02 +0700

    feaure: add config db for dev and pro

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const app = require("./src/app");
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8888;
 const server = app.listen(PORT, () => {
-  console.log("Welcome to my project");
+  console.log("Welcome to my project ", PORT);
 });
 
 // process.on("SIGINT", () => {
